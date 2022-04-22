e9c58165cb9a7a786af4ee2a314b83e8f5bdc8ad
AbuSayedHossain1985 <abusayedhossain1985@gmail.com>
2022-04-22 01:06:28 -0400

    first commit

---
+++ LearnDataType.java
@@ -0,0 +1,13 @@
+package basic1;
+
+public class LearnDataTypy {
+	
+	public static void main(String[]args) {
+		
+		String word="Sayeed";
+		
+		System.out.println("Today's word of the day : " +word);
+		//
+	}
+
+}
---
+++ Variables.java
@@ -0,0 +1,11 @@
+package basic1;
+
+public class Variables {
+	
+	
+	
+//	variable
+//	vary able= value will change based on its need
+//	
+
+}
